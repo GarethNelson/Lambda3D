1d83fdfd01c46fd3e0aa904a29ffa894de917e46
Gareth Nelson <gareth@garethnelson.com>
2016-06-13 10:59:04 +0100

    add cons_cvars.c

---
+++ src/cons_cvars.c
@@ -0,0 +1,56 @@
+//-----------------------------------------------------------------------------
+//
+// $Id:$
+//
+// Copyright (C) 2016 by Gareth Nelson (gareth@garethnelson.com)
+//
+// This file is part of the Lambda engine.
+//
+// The Lambda engine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Lambda engine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+// $Log:$
+//
+// DESCRIPTION:
+//     CVars (Console variables) implementation
+//
+//-----------------------------------------------------------------------------
+
+#include "cons_cvars.h"
+
+struct cons_cvars_t* cvars_ht=NULL;
+
+void dump_cvars() {
+}
+
+void set_cvar_s(char* var_name, char* val) {
+}
+
+void set_cvar_f(char* var_name, float val) {
+}
+
+void set_cvar_i(char* var_name, int val) {
+}
+
+void toggle_cvar(char* var_name) {
+}
+
+char* get_cvar_s(char* var_name) {
+}
+
+float get_cvar_f(char* var_name) {
+}
+
+int   get_cvar_i(char* var_name) {
+}
+
